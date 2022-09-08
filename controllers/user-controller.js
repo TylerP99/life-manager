dd312aa52a6df2df03c5967a026634d56943a8ac
Tyler Perata <tylerperata@gmail.com>
2022-09-07 22:05:48 -0700

    User routes defined

---
+++ controllers/user-controller.js
@@ -0,0 +1,44 @@
+module.exports = {
+    get_signup_page: (req, res, next) => {
+
+    },
+    create_new_user: (req, res, next) => {
+
+    },
+    get_signin_page: (req, res, next) => {
+
+    },
+    authenticate_user: (req, res, next) => {
+
+    },
+    get_forgot_username_page: (req, res, next) => {
+
+    },
+    forgot_username_request: (req, res, next) => {
+
+    },
+    get_forgot_password_page: (req, res, next) => {
+
+    },
+    forgot_password_request: (req, res, next) => {
+
+    },
+    get_forgot_password_change_page: (req, res, next) => {
+
+    },
+    forgot_password_change_request: (req, res, next) => {
+
+    },
+    get_signout_page: (req, res, next) => {
+
+    },
+    signout_user: (req, res, next) => {
+
+    },
+    get_signout_page: (req, res, next) => {
+
+    },
+    change_user_info: (req, res, next) => {
+
+    }
+}
---
+++ routes/apis/users.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+// Sign up page
+router.get("/signup", UserController.get_signup_page);
+
+// Sign up request
+router.post("/create", UserController.create_new_user);
+
+// Sign in page
+router.get("/signin", UserController.get_signin_page);
+
+// Authenticate
+router.post("/authenticate", UserController.authenticate_user);
+
+// Forgot username page
+router.get("/forgotUsername", UserController.get_forgot_username_page);
+
+// Forgot username request
+router.post("/forgotUsername", UserController.forgot_username_request);
+
+// Forgot password page
+router.get("/forgotPassword", UserController.get_forgot_password_page);
+
+// Forgot password request
+router.post("/forgotPassword", UserController.forgot_password_request);
+
+// Forgot password change page
+router.get("/forgotPassword/change", UserController.get_forgot_password_change_page);
+
+// Forgot password change request
+router.put("/forgotPassword/change", UserController.forgot_password_change_request);
+
+// Sign out page
+router.get("/signout", UserController.get_signout_page);
+
+// Sign out request
+router.post("/signout", UserController.signout_user);
+
+// User settings
+router.get("/settings", UserController.get_signout_page);
+
+// Change user information
+router.put("/settings/edit", UserController.change_user_info);

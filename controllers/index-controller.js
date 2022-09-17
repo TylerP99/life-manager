e34e1e98673e838b647b65323a97eff42509e0be
Tyler Perata <tylerperata@gmail.com>
2022-09-16 19:16:10 -0700

    Fix ejs render for tasks

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,8 +1,24 @@
+// Index controller
+
+const Task = require("../models/User");
+
 module.exports = {
     get_landing_page: (req, res) => {
         res.render("landing.ejs");
     },
     get_dashboard: (req, res) => {
         res.render("dashboard.ejs");
+    },
+    get_tasks_page: async (req, res, next) => {
+        // Get user's tasks
+        try {
+            const tasks = await Task.find({owner: req.user.id});
+
+            res.render("tasks.ejs",{tasks:tasks});
+        }
+        catch(e) {
+            console.error(e);
+            next(e);
+        }
     }
 };

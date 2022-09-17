4a6f0f3a3fc37f4e434498dd775eea16f608f139
Tyler Perata <tylerperata@gmail.com>
2022-09-17 16:30:12 -0700

    Add sort function for tasks

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -14,6 +14,23 @@ module.exports = {
         try {
             const tasks = await Task.find({owner: req.user.id});
 
+            tasks.sort((a,b) => {
+
+                if(!a.completed && b.completed) return -1;
+                if(!b.completed && a.completed) return 1;
+
+                if(a.date < b.date) return -1;
+                if(b.date < a.date) return 1;
+
+                if(a.startTime && !b.startTime) return -1;
+                if(b.startTime && !a.startTime) return 1;
+
+                if(a.startTime < b.startTime) return -1;
+                if(b.startTime < a.startTime) return 1;
+
+                return 0;
+            });
+
             res.render("tasks.ejs",{tasks:tasks});
         }
         catch(e) {

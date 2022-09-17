b1b337a7746b90a39280604370327bd2e05d30e3
Tyler Perata <tylerperata@gmail.com>
2022-09-16 18:49:56 -0700

    Add routes to task and goal apis

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+router.use("/task", require("./apis/tasks.js"));
+router.use("/goal", require("./api/goals.js"));
+
 module.exports = router;

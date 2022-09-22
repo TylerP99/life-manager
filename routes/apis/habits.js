6436822e566dfedeb0be5c9cda9195de6e5a8efb
Tyler Perata <tylerperata@gmail.com>
2022-09-22 12:10:44 -0700

    Add id param to request uri

--- routes/apis/habits.js
+++ routes/apis/habits.js
@@ -5,8 +5,8 @@ const HabitController = require("../../controllers/habits-controller");
 
 router.post("/create", HabitController.create_new_habit);
 
-router.put("/update", HabitController.update_habit);
+router.put("/update/:id", HabitController.update_habit);
 
-router.delete("/delete", HabitController.delete_habit);
+router.delete("/delete/:id", HabitController.delete_habit);
 
 module.exports = router;

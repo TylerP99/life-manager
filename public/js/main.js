d7ced9e9969f24f5a16e407e8d09cf04f278af8e
Tyler Perata <tylerperata@gmail.com>
2022-09-23 16:01:40 -0700

    Add form overlay functionality

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,63 @@ if(closeButton) closeButton.forEach(x => x.addEventListener("click", close_item)
 function close_item(e) {
     e.target.parentElement.remove();
 }
+
+
+
+
+// Basic Structure:
+
+// <section>
+
+//     <button class="open">Open</button>
+//     <section class="overlay">
+
+//         <form>
+//             //Inputs and stuff
+//         </form>
+
+//     </section>
+
+// </section>
+
+// Idea: add event listeners to all buttons, use event.target to get parent element, then queryselect for the overlay and open it. All overlays should also have their own event listeners making sure they close if they are clicked
+
+// This code relies heavily on my html structure. Html structure changes should result in some code changes as needed
+
+const formOpenButtons = document.querySelectorAll(".open-button");
+
+formOpenButtons.forEach(x => x.addEventListener("click", open_form));
+
+function open_form(event) {
+    const overlay = event.target.parentElement.querySelector(".overlay");
+    overlay.classList.toggle("hidden");
+    console.log("Open sesame");
+}
+
+
+const formOverlays = document.querySelectorAll(".overlay");
+
+formOverlays.forEach(x => x.addEventListener("click", click_off_form));
+
+function click_off_form(event) {
+    if(!event.target.closest("form")) {
+        console.log("click off close")
+        event.target.classList.toggle("hidden");
+    }
+}
+
+// So far so good, adding to all view pages for testing
+// Amazing... pushing this to github then refactoring the rest of the code
+
+// Really quick, lets add those close buttons to the forms
+const formCloseButtons = document.querySelectorAll(".close-form-button");
+
+formCloseButtons.forEach(x => x.addEventListener("click", close_form));
+
+function close_form(event) {
+    // event is a button whose parent is the form whose parent is the overlay
+    //event.preventDefault();
+    console.log("Button close close")
+    console.log(event.target);
+    event.target.parentElement.parentElement.classList.toggle("hidden");
+}

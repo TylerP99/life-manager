9d754619c3cfb22c58bc04105e59902b4d243cd7
Tyler Perata <tylerperata@gmail.com>
2022-09-24 17:22:47 -0700

    Hopefully express static fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 // DB Connect
 const mongoose = require("mongoose");

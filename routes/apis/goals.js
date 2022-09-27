07a2ef32a78d5ede1646619a8df80a3bad97e6df
Tyler Perata <tylerperata@gmail.com>
2022-09-26 18:03:46 -0700

    Goal routes defined

--- routes/apis/goals.js
+++ routes/apis/goals.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const GoalController = require("../../controllers/goals-controller");
+
+router.post("/create", GoalController.create_new_goal);
+
+router.put("/update/:id", GoalController.update_goal);
+
+router.delete("/delete/:id", GoalController.delete_goal);
+
 module.exports = router;

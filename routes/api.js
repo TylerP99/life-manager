aeef3416c4996d183ac4d0ee4a19454a828022d2
Tyler Perata <tylerperata@gmail.com>
2022-09-26 18:17:09 -0700

    Add routine route

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use("/task", require("./apis/tasks.js"));
 router.use("/goal", require("./apis/goals.js"));
 router.use("/habit", require("./apis/habits.js"));
+router.use("/routine", require("./apis/routines.js"));
 
 module.exports = router;

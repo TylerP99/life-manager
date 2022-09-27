34af201f1ecf2f3062cf30d09990a2e849c3fe17
Tyler Perata <tylerperata@gmail.com>
2022-09-26 18:04:28 -0700

    Routine controller created and methods declared

---
+++ controllers/routines-controller.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+
+    create_new_routine: (req, res, next) => {
+
+    },
+
+    update_routine: (req, res, next) => {
+
+    },
+
+    delete_routine: (req, res, next) => {
+
+    },
+}

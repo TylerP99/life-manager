5baab306c3ee1d46f812aac1397d6140ac6cbede
Tyler Perata <tylerperata@gmail.com>
2022-10-04 11:16:02 -0700

    Add necessary functions for routine rework

--- controllers/routines-reworked.js
+++ controllers/routines-reworked.js
@@ -7,3 +7,55 @@ const Habit = require("../models/Habit.js");
 const Task = require("../models/Task.js");
 
 const HabitController = require("../controllers/routines-controller.js");
+
+module.exports = {
+
+    create_routine_handler: async (req, res, next) => {
+
+    },
+
+    update_routine_handler: async (req, res, next) => {
+
+    },
+
+    delete_routine_handler: async (req, res, next) => {
+
+    },
+
+    add_routine_task_handler: async (req, res, next) => {
+
+    },
+
+    update_routine_task_handler: async (req, res, next) => {
+
+    },
+
+    delete_routine_task_handler: async (req, res, next) => {
+
+    },
+
+    create_routine: async (routine) => {
+
+    },
+
+    update_routine: async (routine, routineID) => {
+
+    },
+
+    delete_routine: async (routineID) => {
+
+    },
+
+    add_routine_task: async (habit, routineID) => {
+
+    },
+
+    update_routine_task: async (habit, habitID, routineID) => {
+
+    },
+
+    delete_routine_task: async (habitID, routineID) => {
+
+    },
+
+};

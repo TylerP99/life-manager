6dc58e09615a07701cab64e57c6fa584d78239cb
Tyler Perata <tylerperata@gmail.com>
2022-10-08 14:19:55 -0700

    Tasks functional again, using new controller pattern

--- controllers/tasks-controller.js
+++ controllers/tasks-controller.js
@@ -1,86 +1,32 @@
 // Task Controller: Handles basic CRUD operations for tasks, generates task page
+const { request } = require("express");
 const Task = require("../models/Task");
 
-module.exports = {
-    // Creates a new task
-    // Received as POST request
-    // Body contains: {
-    //     name
-    //     description (optional)
-    //     date
-    //     startTime (optional)
-    //     endTime (optional)
-    // }
-    create_task: async (req,res,next) => {
-        // Form task object from request
-        req.body.date = req.body.date.split("-");
-        const task = {
-            name: req.body.name,
-            date: new Date(req.body.date[0], req.body.date[1]-1, req.body.date[2]),
-            owner: req.user.id
-        };
+//
+const TaskController = {
 
-        if(req.body.description.length) task.description = req.body.description;
-        if(req.body.startTime.length) {
-            req.body.startTime = req.body.startTime.split(":");
-            task.startTime = new Date( task.date.getFullYear(), task.date.getMonth(), task.date.getDate(), req.body.startTime[0], req.body.startTime[1] );
-        }
-        if(req.body.endTime.length) {
-            req.body.endTime = req.body.endTime.split(":");
-            task.endTime = new Date( task.date.getFullYear(), task.date.getMonth(), task.date.getDate(), req.body.endTime[0], req.body.endTime[1] );
-        }
+    /*
+        Task API Route Hanlders
+    */
+    create_task_handler: async (req, res, next) => {
+        // Format request information into valid task object as shown in Task model (just make them have same datatype)
+        const task = TaskController.format_task_request_form(req.body, req.user);
 
-        // Validate it
-        const errors = validate_task(task);
+        console.log(task);
 
-        if(errors.length) {
-            req.flash("errors", errors)
-            return res.redirect("/tasks");
-        }
-
-        // Save it to database
         try{
-            await Task.create(task);
+            // Create Task in DB (Returns errors if task doesnt pass validation)
+            const errors = await TaskController.create_task(task);
 
-            req.flash("success", "Task successfully created!");
-            res.redirect("/tasks");
-        }
-        catch(e) {
-            console.error(e);
-            next(e);
-        }
-    },
-    update_task: async (req,res,next) => {
-        const taskID = req.params.id;
-        const updatedTask = {};
-        
-        if(req.body.date) req.body.date = req.body.date.split("-");
-
-        try {
-            // Want date for start and end time if user didnt adjust it
-            const task = await Task.findById(taskID);
-
-            if(req.body.name.length) updatedTask.name = req.body.name;
-            if(req.body.description.length) updatedTask.description = req.body.description;
-            (req.body.date) ? updatedTask.date = new Date(req.body.date[0], req.body.date[1]-1, req.body.date[2]) : updatedTask.date = task.date;
-            if(req.body.startTime.length) {
-                req.body.startTime = req.body.startTime.split(":");
-                updatedTask.startTime = new Date( updatedTask.date.getFullYear(), updatedTask.date.getMonth(), updatedTask.date.getDate(), req.body.startTime[0], req.body.startTime[1] );
-            }
-            if(req.body.endTime.length) {
-                req.body.endTime = req.body.endTime.split(":");
-                updatedTask.endTime = new Date( updatedTask.date.getFullYear(), updatedTask.date.getMonth(), updatedTask.date.getDate(), req.body.endTime[0], req.body.endTime[1] );
-            }
-
-            const errors = validate_task(updatedTask);
-            if(errors.length) {
+            // Respond
+            if(errors) { // If there were errors, save them for client side display
                 req.flash("errors", errors);
-                return res.redirect("/tasks");
+            }
+            else { // Otherwise, save a success message
+                req.flash("success", "Task successfully created!");
             }
 
-            await Task.findByIdAndUpdate(taskID, updatedTask, {upsert:false});
-
-            req.flash("success", "Task successfully updated");
+            // Redirect to page to force a reload and display updated info
             res.redirect("/tasks")
         }
         catch(e) {
@@ -88,12 +34,27 @@ module.exports = {
             next(e);
         }
     },
-    mark_complete: async (req,res,next) => {
-        const taskID = req.params.id;
-        try {
-            await Task.findByIdAndUpdate(taskID, {completed: true}, {upsert:false});
+    /*
+        Receives client side PUT request and sends response
+        Request body should contain a full task object (minimum name and date)
+    */
+    update_task_handler: async (req, res, next) => {
+        // Format request (client should send a new object)
+        const task = TaskController.format_task_request_form(req.body, req.user);
+        const id = req.params.id;
 
-            req.flash("success", "Task updated successfully");
+        try{
+            // Call action function
+            const errors = await TaskController.update_task(task, id);
+
+            // Respond
+            if(errors) {
+                req.flash("errors", errors);
+            }
+            else {
+                req.flash("success", "Task successfully updated");
+            }
+
             res.redirect("/tasks");
         }
         catch(e) {
@@ -101,12 +62,21 @@ module.exports = {
             next(e);
         }
     },
-    mark_incomplete: async (req,res,next) => {
-        const taskID = req.params.id;
-        try {
-            await Task.findByIdAndUpdate(taskID, {completed: false}, {upsert:false});
+    mark_complete_handler: async (req, res, next) => {
+        const update = { completed: true };
+        const id = req.params.id;
 
-            req.flash("success", "Task updated successfully");
+        try{
+            const errors = await TaskController.update_task(update,id);
+
+            // Respond
+            if(errors) {
+                req.flash("errors", errors);
+            }
+            else {
+                req.flash("success", "Task successfully updated");
+            }
+
             res.redirect("/tasks");
         }
         catch(e) {
@@ -114,48 +84,148 @@ module.exports = {
             next(e);
         }
     },
-    delete_task: async (req,res,next) => {
-        const taskID = req.params.id;
+    mark_incomplete_handler: async (req, res, next) => {
+        const update = { completed: false };
+        const id = req.params.id;
 
-        try {
-            await Task.findByIdAndDelete(taskID);
+        try{
+            const errors = await TaskController.update_task(update,id);
 
-            req.flash("success", "Task successfully deleted");
+            // Respond
+            if(errors) {
+                req.flash("errors", errors);
+            }
+            else {
+                req.flash("success", "Task successfully updated");
+            }
+
             res.redirect("/tasks");
         }
         catch(e) {
             console.error(e);
             next(e);
         }
-    }
+    },
+    delete_task_handler: async (req, res, next) => {
+        const id = req.params.id;
+
+        try {
+            await TaskController.delete_task(id);
+
+            res.redirect("/tasks");
+        }
+        catch(e) {
+            console.error(e);
+            next(e);
+        }
+    },
+
+    /*
+        Creates a new task in database
+    */
+    create_task: async (task) => {
+        // Validate task
+        const errors = TaskController.validate_task(task);
+
+        if(errors.length) { // If there are errors during validation, return here and dont add task
+            return errors;
+        }
+
+        // Add task to db
+        await Task.create(task);
+
+        return undefined; // Success
+    },
+    /*
+        Updates a target task in database
+        @params:
+            - task: An object containing task items to update
+            - id: A mongoDB id associated to a task document in the database
+        @returns:
+            An array of error messages if there are errors, or undefined if the provided task is valid
+    */
+    update_task: async (task, id) => {
+        const errors = TaskController.validate_task(task);
+
+        if(errors.length) {
+            return errors;
+        }
+
+        await Task.findByIdAndUpdate(id, task);
+
+        return undefined;
+    },
+    delete_task: async (id) => {
+        await Task.findByIdAndDelete(id);
+        return undefined;
+    },
+
+    format_task_request_form: (requestBody, requestUser) => {
+        // Format request information into valid task object as shown in Task model (just make them have same datatype)
+        requestBody.date = requestBody.date.split("-"); // Currently, date is received as YYYY-MM-DD
+        const userDate = new Date(requestBody.userDate); // Hidden input grabs user date as ISO string
+        const task = {
+            name: requestBody.name,
+            owner: requestUser.id,
+        };
+        // Set date information using userDate
+        //task.date = new Date(requestBody.date[0], requestBody.date[1]-1,requestBody.date[2])
+        task.date = new Date(userDate);
+        task.date.setFullYear(requestBody.date[0]);
+        task.date.setMonth(requestBody.date[1]-1);
+        task.date.setDate(requestBody.date[2]);
+        task.date.setHours(0);
+        task.date.setMinutes(0);
+        task.date.setSeconds(0);
+
+        if(requestBody.description.length) task.description = requestBody.description;
+
+        // If Start/End times provided, format and store as date obj
+        if(requestBody.startTime.length) {
+            requestBody.startTime = requestBody.startTime.split(":");
+            task.startTime = new Date(task.date);
+            task.startTime.setHours(requestBody.startTime[0]);
+            task.startTime.setMinutes(requestBody.startTime[1]);
+        }
+        if(requestBody.endTime.length) {
+            requestBody.endTime = requestBody.endTime.split(":");
+            task.endTime = new Date(task.date);
+            task.endTime.setHours(requestBody.endTime[0]);
+            task.endTime.setMinutes(requestBody.endTime[1]);
+        }
+
+        return task;
+    },
+    validate_task: (task) => {
+        const errors = [];
+        const today = new Date(Date.now()); // This will fuck with timezones, fix soon
+        // Check name
+        // Verify name is at most 50 characters
+        if(task.name && task.name.length > 50) errors.push({msg: "Name can't be more than 50 characters long."});
+    
+        // Check description
+        // Verify description is at most 250 characters
+        if(task.description && task.description.length > 250) errors.push({msg: "Description can't be more than 250 characters long."});
+    
+        // Check date
+        // Verify date is on or after today
+        // Date must have a year equal to or greater than current year
+        if( task.date && ( task.date.getFullYear() < today.getFullYear() || // Year is before current year
+        (task.date.getFullYear() == today.getFullYear() && task.date.getMonth() < today.getMonth()) || // Year is current year, but month is before current month
+        (task.date.getFullYear() == today.getFullYear() && task.date.getMonth() == today.getMonth() && task.date.getDate() < today.getDate()) ) ) { // Year is current year, month is current month, date is before current date
+            errors.push({msg: "Date must be on or after the current date."});
+        }
+        // Check startTime
+        // Check endTime
+        // Verify startTime is before endTime
+        if( (task.startTime && task.endTime) && (task.startTime.getHours() > task.endTime.getHours() ||
+        (task.startTime.getHours() == task.endTime.getHours() && task.startTime.getMinutes() > task.endTime.getMinutes() ) ) ) {
+            errors.push({msg: "Start time must be before end time."});
+        }
+    
+        return errors;
+    },
 };
 
-function validate_task(task) {
-    const errors = [];
-    const today = new Date(Date.now());
-    // Check name
-    // Verify name is at most 50 characters
-    if(task.name && task.name.length > 50) errors.push({msg: "Name can't be more than 50 characters long."});
 
-    // Check description
-    // Verify description is at most 250 characters
-    if(task.description && task.description.length > 250) errors.push({msg: "Description can't be more than 250 characters long."});
-
-    // Check date
-    // Verify date is on or after today
-    // Date must have a year equal to or greater than current year
-    if( task.date && ( task.date.getFullYear() < today.getFullYear() || // Year is before current year
-    (task.date.getFullYear() == today.getFullYear() && task.date.getMonth() < today.getMonth()) || // Year is current year, but month is before current month
-    (task.date.getFullYear() == today.getFullYear() && task.date.getMonth() == today.getMonth() && task.date.getDate() < today.getDate()) ) ) { // Year is current year, month is current month, date is before current date
-        errors.push({msg: "Date must be on or after the current date."});
-    }
-    // Check startTime
-    // Check endTime
-    // Verify startTime is before endTime
-    if( (task.startTime && task.endTime) && (task.startTime.getHours() > task.endTime.getHours() ||
-    (task.startTime.getHours() == task.endTime.getHours() && task.startTime.getMinutes() > task.endTime.getMinutes() ) ) ) {
-        errors.push({msg: "Start time must be before end time."});
-    }
-
-    return errors;
-}
+module.exports = TaskController;
--- routes/apis/tasks.js
+++ routes/apis/tasks.js
@@ -7,18 +7,18 @@ const router = express.Router();
 const TaskController = require("../../controllers/tasks-controller.js");
 
 // Create new task
-router.post("/create", TaskController.create_task);
+router.post("/create", TaskController.create_task_handler);
 
 // Update task properties
-router.put("/update/:id", TaskController.update_task);
+router.put("/update/:id", TaskController.update_task_handler);
 
 // Mark task as complete
-router.put("/markComplete/:id", TaskController.mark_complete);
+router.put("/markComplete/:id", TaskController.mark_complete_handler);
 
 // Mark task as incomplete
-router.put("/markIncomplete/:id", TaskController.mark_incomplete);
+router.put("/markIncomplete/:id", TaskController.mark_incomplete_handler);
 
 // Delete task
-router.delete("/delete/:id", TaskController.delete_task);
+router.delete("/delete/:id", TaskController.delete_task_handler);
 
 module.exports = router;

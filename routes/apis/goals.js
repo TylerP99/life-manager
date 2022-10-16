890be48130d96403ea61445ed042e87d3ae059de
Tyler Perata <tylerperata@gmail.com>
2022-10-15 20:57:17 -0700

    Add other goal apis

--- routes/apis/goals.js
+++ routes/apis/goals.js
@@ -5,8 +5,23 @@ const GoalController = require("../../controllers/goals-controller");
 
 router.post("/create", GoalController.create_new_goal_handler);
 
+router.put("/addTask/:id", GoalController.add_new_task_handler);
+
+router.put("/addHabit/:id", GoalController.add_new_habit_handler);
+
+router.put("/addRoutine/:id", GoalController.add_new_routine_handler);
+
+// TODO
+router.put("/addGoal/:id", GoalController.add_new_goal_handler);
+
 router.put("/update/:id", GoalController.update_goal_handler);
 
 router.delete("/delete/:id", GoalController.delete_goal_handler);
+
+router.delete("/deleteTask/:id", GoalController.delete_task_handler);
+
+router.delete("/deleteHabit/:id", GoalController.delete_habit_handler);
+
+router.delete("/deleteRoutine/:id", GoalController.delete_routine_handler);
 
 module.exports = router;

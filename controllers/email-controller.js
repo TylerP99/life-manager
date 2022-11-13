c932caff2e8fd73b8059ea7bbe590426f910a1e2
Tyler Perata <tylerperata@gmail.com>
2022-11-13 00:43:49 -0800

    Add email controller

---
+++ controllers/email-controller.js
@@ -0,0 +1,57 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    }
+});
+
+const {DateTime} = require("luxon");
+const Task = require("../models/Task");
+
+const EmailController = {
+
+    send_reminder: async (taskID) => {
+        // Get task from db
+        const task = await Task.findById(taskID);
+
+        // Get user from db
+        const user = await User.findByID(task.owner);
+
+        // Format message
+        const greeting = `Hello ${user.username}!\n\n`;
+        const content = "You have an upcoming task:" + 
+                        "\n" + 
+                        task.name +         
+                        ((task.startTime) ? `at ${DateTime.fromJSDate(task.startTime, {zone: user.timezone}).toLocaleString(DateTime.TIME_SIMPLE)}` : "") + 
+                        " on " + 
+                        DateTime.fromJSDate(task.date, {zone: user.timezone}).toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY) + 
+                        ((task.description) ? "\n" + task.description : "" )
+        const message = greeting + content;
+
+        const mailOptions = {
+            from: process.env.MAIL_USERNAME,
+            to: user.email,
+            subject: "Upcoming Task",
+            text: message,
+        };
+
+        transporter.sendMail(mailOptions, (err) => {
+            if(err) {
+                console.error(err);
+            }
+            else {
+                console.log("Mail sent");
+            }
+        });
+    },
+    
+};
+
+module.exports = EmailController;

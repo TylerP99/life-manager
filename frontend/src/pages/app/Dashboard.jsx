93211c579b99bd25bf4cf872a4b6885d87e4692c
Tyler Perata <tylerperata@gmail.com>
2022-12-24 00:42:36 -0800

    Frontend structuring

---
+++ frontend/src/pages/app/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <section>
+        {/* Upcoming tasks */}
+        {/* Main Goals */}
+      </section>
+    </div>
+  )
+}
+
+export default Dashboard

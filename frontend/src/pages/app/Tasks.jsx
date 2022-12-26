24e7b05c99becbf96ea494109d1c744993241280
Tyler Perata <tylerperata@gmail.com>
2022-12-25 22:04:54 -0800

    Task page extension and styling

--- frontend/src/pages/app/Tasks.jsx
+++ frontend/src/pages/app/Tasks.jsx
@@ -1,25 +1,83 @@
-import React from 'react'
+import {useState} from 'react'
+
+import {FaArrowAltCircleRight, FaArrowAltCircleDown} from "react-icons/fa";
 
 function Tasks() {
 
-  const completeTasks = [
+  const [completeDropdownOpen, setCompleteDropdownOpen] = useState(false);
+  const [overdueDropdownOpen, setOverdueDropdownOpen] = useState(false);
+  const [incompleteDropdownOpen, setIncompleteDropdownOpen] = useState(false);
 
-  ];
-
-  const overdueTasks = [];
-
-  const incompleteTasks = [];
+  const toggleComplete = () => setCompleteDropdownOpen(!completeDropdownOpen);
+  const toggleOverdue = () => setOverdueDropdownOpen(!overdueDropdownOpen);
+  const toggleIncomplete = () => setIncompleteDropdownOpen(!incompleteDropdownOpen);
 
   return (
-    <div>
-      <h1>Tasks</h1>
+    <div className='w-full border-8 border-red-500'>
+      <header
+      className='flex flex-col mb-5 shadow-md md:flex-row'
+      >
+        <h1
+        className="w-full text-4xl"
+        >
+          Tasks
+        </h1>
+        <section>
+          <section>Forms</section>
+        </section>
+      </header>
       <section>
-        <section>Forms</section>
-      </section>
-      <section>
-        <section>Complete tasks dropdown section</section>
-        <section>Overdue tasks dropdown section</section>
-        <section>Upcoming tasks dropdown section</section>
+        <section
+        className="mb-5 text-lg"
+        >
+          <section 
+          className="flex justify-center items-center gap-10 border-4 rounded-md mx-auto py-3 cursor-pointer w-[95%]"
+          onClick={toggleComplete}
+          >
+            <div>
+              {completeDropdownOpen ? 
+              <FaArrowAltCircleDown />
+              : 
+              <FaArrowAltCircleRight /> 
+              }
+            </div>
+            Completed Tasks
+          </section>
+        </section>
+        <section
+        className="mb-5 text-lg"
+        >
+          <section 
+            className="flex justify-center items-center gap-10 border-4 rounded-md mx-auto py-3 cursor-pointer w-[95%]"
+            onClick={toggleOverdue}
+            >
+              <div>
+                {overdueDropdownOpen ? 
+                <FaArrowAltCircleDown />
+                : 
+                <FaArrowAltCircleRight /> 
+                }
+              </div>
+              Overdue Tasks
+            </section>
+          </section>
+        <section
+        className="mb-5 text-lg"
+        >
+          <section 
+            className="flex justify-center items-center gap-10 border-4 rounded-md mx-auto py-3 cursor-pointer w-[95%]"
+            onClick={toggleIncomplete}
+            >
+              <div>
+                {incompleteDropdownOpen ? 
+                <FaArrowAltCircleDown />
+                : 
+                <FaArrowAltCircleRight /> 
+                }
+              </div>
+              Upcoming Tasks
+            </section>
+        </section>
       </section>
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        
+      }
+    },
   },
   plugins: [],
 }

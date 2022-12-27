ce5111c3b46383bbcdf6a19bf2f8e6bbc06fbf3f
Tyler Perata <tylerperata@gmail.com>
2022-12-26 22:45:05 -0800

    Mobile and desktop nav

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -4,18 +4,49 @@ import {Outlet, NavLink} from "react-router-dom";
 
 function AppLayout() {
 
+    const navLinkStyle = "w-full py-5 pl-7 pr-24 border";
+    const navLinkActive = "bg-slate-100"
+
+    const [navOpen, setNavOpen] = useState(false);
+
+    const toggleNavOpen = () => setNavOpen(!navOpen);
+
     return (
         <section className='flex w-full'>
-            <section className='sticky top-0 left-0 h-screen border-r w-1/6'>
-                <nav className='flex flex-col h-full'>
-                    <NavLink className={({isActive}) => "w-full py-5 pl-7 pr-24 border" + (isActive ? " bg-slate-100" : "")} to="/settings" >Settings</NavLink>
-                    <NavLink exact className={({isActive}) => "w-full py-5 pl-7 pr-24 border" + (isActive ? " bg-slate-100" : "")} to="/dashboard" >Dashboard</NavLink>
-                    <NavLink className={({isActive}) => "w-full py-5 pl-7 pr-24 border" + (isActive ? " bg-slate-100" : "")} to="/goals" >Goals</NavLink>
-                    <NavLink className={({isActive}) => "w-full py-5 pl-7 pr-24 border" + (isActive ? " bg-slate-100" : "")} to="/routines" >Routines</NavLink>
-                    <NavLink className={({isActive}) => "w-full py-5 pl-7 pr-24 border" + (isActive ? " bg-slate-100" : "")} to="/habits" >Habits</NavLink>
-                    <NavLink className={({isActive}) => "w-full py-5 pl-7 pr-24 border" + (isActive ? " bg-slate-100" : "")} to="/tasks" >Tasks</NavLink>
-                    <NavLink className={({isActive}) => "w-full py-5 pl-7 pr-24 border" + (isActive ? " bg-slate-100" : "")} to="/signout">Sign Out</NavLink>
-                </nav>
+            <section id='navigation'>
+                <section id='mobile-nav' className='md:hidden'>
+                    <nav className={'flex flex-col fixed top-0 left-0 bg-white h-screen border-r transition-transform md:w-1/6 md:sticky md:transition-none' + ((navOpen) ? "" : " -translate-x-full")}>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/settings" >Settings</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/dashboard" >Dashboard</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/goals" >Goals</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/routines" >Routines</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/habits" >Habits</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/tasks" >Tasks</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/signout">Sign Out</NavLink>
+                    </nav>
+                    <div className='absolute top-[2%] left-[90%]'>
+                        <button 
+                            id="menu-btn" 
+                            className={"block hamburger md:hidden focus:outline-none" + ((navOpen) ? " open" : "")}
+                            onClick={toggleNavOpen}
+                            >
+                            <div className="hamburger-top"></div>
+                            <div className="hamburger-middle"></div>
+                            <div className="hamburger-bottom"></div>
+                        </button>
+                    </div>
+                </section>
+                <section id='normal-nav'>
+                    <nav className={'flex-col hidden sticky top-0 left-0 bg-white h-screen border-r md:flex'}>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/settings" >Settings</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/dashboard" >Dashboard</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/goals" >Goals</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/routines" >Routines</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/habits" >Habits</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/tasks" >Tasks</NavLink>
+                        <NavLink className={({isActive}) => navLinkStyle + (isActive ? " " + navLinkActive : "")} to="/signout">Sign Out</NavLink>
+                    </nav>
+                </section>
             </section>
             <Outlet/>
         </section>
